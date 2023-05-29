88a2374ae6cbcf1fafed844904704465533b5bc8
LagutamS <LagutamS@IMCPBCP37-BLL.mumbai1.corp.citiustech.com>
2023-05-29 13:03:36 +0530

    commit 05/29

---
+++ JavaBasic0/src/main/java/Basics/DataTypes.java
@@ -0,0 +1,38 @@
+package Basics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Byte bi = 126;
+		System.out.println("The Data type is Byte with value:" + bi);
+		
+		short sh = 3600;
+		System.out.println("The Data type is Short with value:" + sh);
+		
+		int in = -23896;
+		System.out.println("The Data type is Int with value:" + in);
+		
+		long lg = 1242328498;
+		System.out.println("The Data type is Long with value:" + lg);
+		
+		//Need to mention literal 'F' in the value for float data type
+		float ft = 10.324F;
+		System.out.println("The Data type is Float with value:" + ft);
+		
+		double db = 12.0934565;
+		System.out.println("The Data type is Double with value:" + db);
+		
+		char ch = 'A';
+		System.out.println("The Data type is Character with value:" + ch);
+		
+		String st = "Sample String";
+		System.out.println("The Data type is String with value:" + st);
+		
+		Boolean x = true;
+		System.out.println("The Data type is Boolean with value:" + x);
+		
+				
+	}
+
+}
